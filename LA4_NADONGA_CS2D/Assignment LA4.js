f138cb64e069c1888625b39a580f8452924fc3f5
Mikhaella Nadonga <soriaomikhaella@gmail.com>
2024-12-09 16:37:12 +0800

    Add files via upload

---
+++ LA4_NADONGA_CS2D/Assignment LA4.js
@@ -0,0 +1,41 @@
+// create a program simple queueing sytem for costumer service.
+// serving 5 costumers, Elaine,Althea,Angelo,Lito,Engelbert.
+
+let costumer = ["Elaine 1","Althea 2","Angelo 3","Lito 4","Engelbert 5"];
+               //--io--    --i1--        --i2--   --i3--   --i4--
+
+ // after sorting the costumer's name ,the program must ask for a number o be served
+ let  = costumer.toSorted();
+ console.log(costumer);
+ console.log("first costumer:");
+ console.log(costumer[0]);
+ 
+ // remove elaine inside the array:
+ costumer.shift();
+ console.log(costumer);
+ console.log("second costumer:" );
+ console.log(costumer[0]);
+
+ //remove althea inside the array;
+ costumer.shift();
+ console.log(costumer);
+ console.log("third costumer:");
+ console.log(costumer[0]);
+
+ //remove angelo inside the array
+ costumer.shift();
+ console.log(costumer);
+ console.log("fourth costumer:");
+ console.log(costumer[0]);
+
+ //remove lito insde the array
+ costumer.shift();
+ console.log(costumer);
+ console.log("fifth costumer");
+ console.log(costumer[0]);
+
+ 
+ 
+ 
+ 
+
